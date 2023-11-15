d0de33de76c80b9e0d6ab90cc83ae6dfff023c52
Your Name <you@example.com>
2023-11-15 17:20:30 +0400

    Initial third  commit of the project

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,2 +1,30 @@
-package org.example;public class Person {
+package org.example;
+
+public class Person extends SecondClass{
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/SecondClass.java
+++ src/main/java/org/example/SecondClass.java
@@ -1,2 +1,20 @@
-package org.example;public class SecondClass {
+package org.example;
+
+public class SecondClass {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "SecondClass{" +
+                "id=" + id +
+                '}';
+    }
 }
